a2bc333693dbca56ad779475b89a6fe84dffac56
Ismael Salas López <isma.salas@protonmail.com>
2020-08-03 22:42:50 -0500

    Aplication class.

--- source/Aplicacion.h
+++ source/Aplicacion.h
@@ -8,12 +8,15 @@
 
 const std::array< std::string, 12 > meses{ "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" };
 
+void establecerUsuario( GtkWidget *widget, gpointer ptr );
+
 class Aplicacion
 {
     public:
         Aplicacion();
         int iniciar();
-		Widget *obtenerWidget() const;
+        Widget *obtenerWidget() const;
+        void autenticarUsuario( const std::string nombreUsuario, const std::string contrasena );
 
     private:
         Usuario usuario();

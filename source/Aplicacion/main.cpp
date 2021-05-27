3c1ccd22816c04578b950718dc64cce0ad1f1e79
Ismael Salas López <isma.salas@protonmail.com>
2021-05-27 15:42:55 -0500

    Irrelevante.

--- source/Aplicacion/main.cpp
+++ source/Aplicacion/main.cpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 using namespace std;
 
+
+
 void actualizarTiempo( GtkWidget *widget, gpointer ptr );
 void establecerUsuario( GtkWidget *widget, gpointer ptr );
 
@@ -16,7 +18,7 @@ int main( int argc, char *argv[] )
         // Crea la aplicación 
         GtkApplication *aplicacion = gtk_application_new( "mx.com.minipluss.scb", G_APPLICATION_FLAGS_NONE );
         g_signal_connect( aplicacion, "activate", G_CALLBACK( iniciar ), nullptr );
-        g_application_run( G_APPLICATION( aplicacion ), argc, argv ); 
+        g_application_run( G_APPLICATION( aplicacion ), argc, argv );
         g_object_unref( aplicacion );
     }
     catch( exception &e ){
@@ -37,33 +39,4 @@ int main( int argc, char *argv[] )
     }
 
     return 0; 
-}
-
-gboolean
-draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
-{
-  guint width, height;
-  GdkRGBA color;
-  GtkStyleContext *context;
-
-  context = gtk_widget_get_style_context (widget);
-
-  width = gtk_widget_get_allocated_width (widget);
-  height = gtk_widget_get_allocated_height (widget);
-
-  gtk_render_background (context, cr, 0, 0, width, height);
-
-  cairo_arc (cr,
-             width / 2.0, height / 2.0,
-             MIN (width, height) / 2.0,
-             0, 2 * G_PI);
-
-  gtk_style_context_get_color (context,
-                               gtk_style_context_get_state (context),
-                               &color);
-  gdk_cairo_set_source_rgba (cr, &color);
-
-  cairo_fill (cr);
-
- return FALSE;
 }
